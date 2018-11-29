631687c8570aed5eeb722bcddd7a237d48ae13f5
Christian Amor Kvalheim <christkv@gmail.com>
2018-11-29 15:46:04 +0100

    - Adding legacy delete message parsing

--- recommendation/src/main/kotlin/com/mconsulting/indexrecommender/profiling/Delete.kt
+++ recommendation/src/main/kotlin/com/mconsulting/indexrecommender/profiling/Delete.kt
@@ -1,17 +1,41 @@
 package com.mconsulting.indexrecommender.profiling
 
+import com.beust.klaxon.JsonArray
 import com.beust.klaxon.JsonObject
 
-data class DeleteCommand(val db: String, val collection: String, val query: JsonObject)
+data class DeleteCommand(val db: String, val collection: String, val query: JsonArray<JsonObject>)
 
 class Delete(doc: JsonObject) : WriteOperation(doc) {
     fun command() : DeleteCommand {
+        if (doc.containsKey("command")) {
+            return modernDelete(doc)
+        } else {
+            return legacyDelete(doc)
+        }
+    }
+
+    private fun legacyDelete(doc: JsonObject): DeleteCommand {
+        val queryObject: JsonObject
+
+        if (doc.containsKey("query")) {
+            queryObject = doc.obj("query")!!
+        } else {
+            queryObject = JsonObject()
+        }
+
+        return DeleteCommand(
+            namespace().db,
+            namespace().collection,
+            JsonArray(listOf(queryObject)))
+    }
+
+    private fun modernDelete(doc: JsonObject): DeleteCommand {
         val command = doc.obj("command")!!
 
         return DeleteCommand(
             namespace().db,
             namespace().collection,
-            command.obj("q")!!)
+            command.array<JsonObject>("deletes")!!)
     }
 
     fun numberDeleted() = getInt("ndeleted", doc)
