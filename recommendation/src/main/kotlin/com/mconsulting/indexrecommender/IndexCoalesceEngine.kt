cadc51edb46a47dd2b43aeb619d7de9e72d4cf99
Christian Amor Kvalheim <christkv@gmail.com>
2018-12-06 23:10:36 +0100

    - Merge Text Index and Compound Text Index correctly

--- recommendation/src/main/kotlin/com/mconsulting/indexrecommender/IndexCoalesceEngine.kt
+++ recommendation/src/main/kotlin/com/mconsulting/indexrecommender/IndexCoalesceEngine.kt
@@ -1,6 +1,9 @@
 package com.mconsulting.indexrecommender
 
+import com.mconsulting.indexrecommender.indexes.CompoundIndex
+import com.mconsulting.indexrecommender.indexes.CompoundTextIndex
 import com.mconsulting.indexrecommender.indexes.Index
+import com.mconsulting.indexrecommender.indexes.IndexDirection
 import com.mconsulting.indexrecommender.indexes.MultiFieldIndex
 import com.mconsulting.indexrecommender.indexes.SingleFieldIndex
 import com.mconsulting.indexrecommender.indexes.TextField
@@ -21,6 +24,7 @@ class IndexCoalesceEngine {
         val singleFieldIndexes = candidateIndexes.filterIsInstance<SingleFieldIndex>()
         val multiFieldIndexes = candidateIndexes.filterIsInstance<MultiFieldIndex>()
         val textIndexes = candidateIndexes.filterIsInstance<TextIndex>()
+        val compoundTextIndexes = candidateIndexes.filterIsInstance<CompoundTextIndex>()
 
         // Any leftover indexes
         val restOfIndexes = candidateIndexes.filter {
@@ -32,32 +36,94 @@ class IndexCoalesceEngine {
         // Check if the single field indexes are contained in the compound indexes
         processSingleFieldIndexes(singleFieldIndexes, multiFieldIndexes, removedIndexes, indexes)
 
+        // Process text indexes
+        processTextIndexes(textIndexes, compoundTextIndexes, removedIndexes, indexes)
+
         // Process compound indexes
         processCompoundIndexes(multiFieldIndexes, removedIndexes, indexes)
-
-        // Process text indexes
-        processTextIndexes(textIndexes, removedIndexes, indexes)
 
         // Return the indexes
         return IndexCoalesceResult(
             indexes + restOfIndexes, removedIndexes)
     }
 
-    private fun processTextIndexes(textIndexes: List<TextIndex>, removedIndexes: MutableList<Index>, indexes: MutableList<Index>) {
-        if (textIndexes.isEmpty()) return
+    private fun processTextIndexes(textIndexes: List<TextIndex>, compoundTextIndexes: List<CompoundTextIndex>, removedIndexes: MutableList<Index>, indexes: MutableList<Index>) {
+        // Do we have any text indexes
+        if (textIndexes.isNotEmpty()) {
+            val textIndexQueue = LinkedList(textIndexes.sortedBy { it.fields.size })
+            val mergedFields = mutableSetOf<TextField>()
 
-        val textIndexQueue = LinkedList(textIndexes.sortedBy { it.fields.size })
-        val mergedFields = mutableListOf<TextField>()
+            // Merged the basic text index fields
+            textIndexQueue.forEach {
+                mergedFields += it.fields.filter { !mergedFields.contains(it) }
+                removedIndexes += it
+            }
 
-        textIndexQueue.forEach {
-            mergedFields += it.fields.filter { !mergedFields.contains(it) }
-            removedIndexes += it
+            // Go over all the compound text indexes
+            if (compoundTextIndexes.isNotEmpty()) {
+                mergedFields += mergeCompoundIndexes(compoundTextIndexes, indexes)
+//
+//                // We need to merge any missing text fields
+//                mergedFields += compoundTextIndexes.map {
+//                    it.fields.filter { !mergedFields.contains(it) }
+//                }.flatten()
+//
+//                // Create a candidate compound index and add it if it does
+//                // not already exist, we are in fact splitting indexes here
+//                // due to there only being possible to have one text index per collection
+//                compoundTextIndexes.forEach {
+//                    val compoundIndex = CompoundIndex(it.compoundFields.map {
+//                        "${it.name}_${it.direction.value()}"
+//                    }.joinToString("_"), it.compoundFields)
+//
+//                    if (!indexes.contains(compoundIndex)) {
+//                        indexes += compoundIndex
+//                    }
+//                }
+            }
+
+            // Merge any compound text indexes, splitting them and merging them
+            indexes += TextIndex(createIndexName(mergedFields.toList()), mergedFields.toList())
+            return
         }
 
-        indexes += TextIndex(createIndexName(mergedFields), mergedFields)
+        // Do we have more than one compound index, merge them
+        if (compoundTextIndexes.size > 1) {
+            val mergedFields = mergeCompoundIndexes(compoundTextIndexes, indexes)
+            indexes += TextIndex(createIndexName(mergedFields.toList()), mergedFields.toList())
+        }
     }
 
-    private fun createIndexName(mergedFields: MutableList<TextField>): String {
+    private fun mergeCompoundIndexes(compoundTextIndexes: List<CompoundTextIndex>, indexes: MutableList<Index>): List<TextField> {
+        val mergedFields = mutableListOf<TextField>()
+        // We need to merge any missing text fields
+        mergedFields += compoundTextIndexes.map {
+            it.fields.filter { !mergedFields.contains(it) }
+        }.flatten()
+
+        compoundTextIndexes.forEach {
+            val filteredFields = it.compoundFields.filter {
+                it.name !in listOf("_fts", "_ftsx")
+            }
+            // Create a candidate compound index and add it if it does
+            // not already exist, we are in fact splitting indexes here
+            // due to there only being possible to have one text index per collection
+            val compoundIndex = CompoundIndex(filteredFields.map {
+                "${it.name}_${when (it.direction) {
+                    IndexDirection.ASCENDING, IndexDirection.DESCENDING -> it.direction.value()
+                    else -> 1
+                }}"
+            }.joinToString("_"), filteredFields)
+
+            if (!indexes.contains(compoundIndex)) {
+                indexes += compoundIndex
+            }
+        }
+
+        return mergedFields
+    }
+
+    private fun createIndexName(mergedFields: List<TextField>): String {
         return mergedFields.map { "${it.path.joinToString(".")}_${it.weight}" }.joinToString("_")
     }
 
--- recommendation/src/test/kotlin/com/mconsulting/indexrecommender/TextIndexCoalesceEngineTest.kt
+++ recommendation/src/test/kotlin/com/mconsulting/indexrecommender/TextIndexCoalesceEngineTest.kt
@@ -1,5 +1,9 @@
 package com.mconsulting.indexrecommender
 
+import com.mconsulting.indexrecommender.indexes.CompoundIndex
+import com.mconsulting.indexrecommender.indexes.CompoundTextIndex
+import com.mconsulting.indexrecommender.indexes.Field
+import com.mconsulting.indexrecommender.indexes.IndexDirection
 import com.mconsulting.indexrecommender.indexes.TextField
 import com.mconsulting.indexrecommender.indexes.TextIndex
 import org.junit.jupiter.api.Test
@@ -41,7 +45,106 @@ class TextIndexCoalesceEngineTest {
 
     @Test
     fun shouldCorrectlyHandleCompoundTextIndexCombo() {
-        // TODO What to do with compound field + text index combo
+        val indexes = coalesceEngine.coalesce(listOf(
+            TextIndex("a", listOf(
+                TextField(listOf("c"), 1)
+            )),
+            CompoundTextIndex("b", listOf(
+                Field("a", IndexDirection.ASCENDING),
+                Field("b", IndexDirection.DESCENDING),
+                Field("_fts", IndexDirection.UNKNOWN),
+                Field("_ftsx", IndexDirection.ASCENDING)
+            ), listOf(
+                TextField(listOf("d"), 10)
+            ))
+        ))
+
+        assertEquals(2, indexes.indexes.size)
+        assertEquals(2, indexes.removedIndexes.size)
+
+        assertEquals(listOf(
+            CompoundIndex("a_1_b_-1", listOf(
+                Field("a", IndexDirection.ASCENDING),
+                Field("b", IndexDirection.DESCENDING)
+            )),
+            TextIndex("c_1_d_10", listOf(
+                TextField(listOf("c"), 1),
+                TextField(listOf("d"), 10)
+            ))
+        ), indexes.indexes)
+
+        assertEquals(listOf(
+            TextIndex("a", listOf(
+                TextField(listOf("c"), 1)
+            )),
+            CompoundTextIndex("b", listOf(
+                Field("a", IndexDirection.ASCENDING),
+                Field("b", IndexDirection.DESCENDING),
+                Field("_fts", IndexDirection.UNKNOWN),
+                Field("_ftsx", IndexDirection.ASCENDING)
+            ), listOf(
+                TextField(listOf("d"), 10)
+            ))
+        ), indexes.removedIndexes)
+    }
+
+    @Test
+    fun shouldMergeTwoCompoundTextIndexes() {
+        val indexes = coalesceEngine.coalesce(listOf(
+            CompoundTextIndex("a", listOf(
+                Field("c", IndexDirection.ASCENDING),
+                Field("b", IndexDirection.DESCENDING),
+                Field("_fts", IndexDirection.UNKNOWN),
+                Field("_ftsx", IndexDirection.ASCENDING)
+            ), listOf(
+                TextField(listOf("e"), 10)
+            )),
+            CompoundTextIndex("b", listOf(
+                Field("a", IndexDirection.ASCENDING),
+                Field("b", IndexDirection.DESCENDING),
+                Field("_fts", IndexDirection.UNKNOWN),
+                Field("_ftsx", IndexDirection.ASCENDING)
+            ), listOf(
+                TextField(listOf("d"), 10)
+            ))
+        ))
+
+        assertEquals(3, indexes.indexes.size)
+        assertEquals(2, indexes.removedIndexes.size)
+
+        assertEquals(listOf(
+            CompoundIndex("c_1_b_-1", listOf(
+                Field("c", IndexDirection.ASCENDING),
+                Field("b", IndexDirection.DESCENDING)
+            )),
+            CompoundIndex("a_1_b_-1", listOf(
+                Field("a", IndexDirection.ASCENDING),
+                Field("b", IndexDirection.DESCENDING)
+            )),
+            TextIndex("e_10_d_10", listOf(
+                TextField(listOf("e"), 10),
+                TextField(listOf("d"), 10)
+            ))
+        ), indexes.indexes)
+
+        assertEquals(listOf(
+            CompoundTextIndex("a", listOf(
+                Field("c", IndexDirection.ASCENDING),
+                Field("b", IndexDirection.DESCENDING),
+                Field("_fts", IndexDirection.UNKNOWN),
+                Field("_ftsx", IndexDirection.ASCENDING)
+            ), listOf(
+                TextField(listOf("e"), 10)
+            )),
+            CompoundTextIndex("b", listOf(
+                Field("a", IndexDirection.ASCENDING),
+                Field("b", IndexDirection.DESCENDING),
+                Field("_fts", IndexDirection.UNKNOWN),
+                Field("_ftsx", IndexDirection.ASCENDING)
+            ), listOf(
+                TextField(listOf("d"), 10)
+            ))
+        ), indexes.removedIndexes)
     }
 
 //    @Test
